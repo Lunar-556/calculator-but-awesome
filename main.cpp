5781ffa18150a27a3f9b6dbf85db29947972aa34
Lunar-556 <76506594+Lunar-556@users.noreply.github.com>
2020-12-23 00:06:02 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+char car1;
+
+double  num1;
+double  num2;
+
+
+cout << "enter your first number. ";
+
+cin >> num1 ;
+
+cout << "do you want to multiply, divide, add or subtract? ";
+cin >> car1 ;
+cout << "enter your second number. ";
+cin >> num2;
+cout << "  " << endl;
+cout << "do the math yourself fucking retard.";
+
+    return 0;
+}
